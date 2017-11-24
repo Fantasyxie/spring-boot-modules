df1384b033d32a084ae53ee3bdb1e8f89081dd43
Fantasy_Xie <cxhfy@aliyun.com>
2017-11-24 15:09:07 +0800

    init5

---
+++ web/src/main/java/fantasy/modules/ModulesApplication.java
@@ -0,0 +1,15 @@
+package fantasy.modules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 类说明：
+ * Created by fantasyxie on 2017/11/24.
+ */
+@SpringBootApplication
+public class ModulesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ModulesApplication.class, args);
+    }
+}
